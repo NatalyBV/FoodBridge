2b711b9e8904becd0280a3529d677fde1498c96d
capmorganbig <bogdan.rybalka.3@gmail.com>
2020-10-13 20:18:06 +0300

    Updated stack name

--- front-end/infrastructure/bin/front-end.ts
+++ front-end/infrastructure/bin/front-end.ts
@@ -2,6 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { FrontEndStack } from '../lib/front-end-stack';
+import { Properties } from '../lib/properties';
 
 const app = new cdk.App();
-new FrontEndStack(app, 'InfrastructureStack');
+new FrontEndStack(app, Properties.STACK_NAME);
